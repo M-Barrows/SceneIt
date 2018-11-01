284cbdd219cebcd55f10363a2590bfa58330c886
BarrowsMA <m.barrows@live.com>
2018-11-01 19:28:47 -0400

    Initial Commit

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TVTracker.Models;
+
+namespace TVTracker.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+        {
+        }
+    }
+}
